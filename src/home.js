99f5630efeaf61f7e197f610afca2eca787e08a1
Anton Hallstrom <antonhallstrom@live.se>
2020-01-02 20:02:55 +0100

    home animation

--- src/home.js
+++ src/home.js
@@ -15,14 +15,6 @@ import styled from '@emotion/styled'
 // box-shadow: 0 0 30px 15px #fff, /* inner white */ 0 0 50px 30px #f0f,
 // /* middle magenta */ 0 0 70px 45px #0ff; /* outer cyan */
 
-const Diamond = styled(Box)`
-  width: 32px;
-  height: 32px;
-  background-color: white;
-  transform: rotate(45deg);
-  position: absolute;
-`
-
 const DiamondMotion = styled(motion.div)`
   width: 32px;
   height: 32px;
@@ -30,139 +22,192 @@ const DiamondMotion = styled(motion.div)`
   position: absolute;
 `
 
+// #363540
+const Background = styled(motion.div)`
+  height: 500px;
+  width: 100%;
+  background: radial-gradient(
+    circle,
+    rgba(255, 255, 255, 0.8) 18%,
+    rgba(0, 255, 255, 0.8) 52%,
+    rgba(255, 0, 255, 1) 100%
+  );
+`
+
 export function Home() {
   return (
     <Route exact path="/">
-      <Box height="500px" bg="#363540" width="100%">
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          height="100%"
+      <Box bg="#363540">
+        <Background
+          animate={{
+            background: [
+              `radial-gradient(
+              circle,
+              #363540 18%,
+              #363540 52%,
+              #363540 100%
+            )`,
+              `radial-gradient(
+              circle,
+              rgba(0, 0, 0, 0.8) 18%,
+              rgba(0, 0, 0, 0.8) 52%,
+              rgba(0, 0, 0, 1) 100%
+            )`,
+              `radial-gradient(
+              circle,
+              rgba(0, 0, 0, 0.2) 9%,
+              rgba(0, 0, 0, 0.2) 21%,
+              rgba(0, 0, 0, 0.2) 50%
+            )`,
+              `radial-gradient(
+              circle,
+              rgba(255, 255, 255, 0.8) 18%,
+              rgba(0, 255, 255, 0.8) 52%,
+              rgba(255, 0, 255, 1) 100%
+            )`,
+              `radial-gradient(
+              circle,
+              rgba(255, 255, 255, 0) 9%,
+              rgba(0, 255, 255, 0) 21%,
+              rgba(255, 0, 255, 0) 50%
+            )`,
+            ],
+          }}
+          transition={{
+            delay: 0.2,
+            duration: 4,
+            ease: 'easeInOut',
+          }}
         >
-          <Box position="relative">
-            <DiamondMotion
-              initial={{
-                left: '-22px',
-                bottom: '-58px',
-                scale: 1,
-              }}
-              animate={{
-                rotate: [45, 45, 90, 225, 225],
-                left: '-22px',
-                bottom: '-58px',
-                opacity: [1, 0.5, 1, 1, 1],
-                scale: [0.25, 0.5, 0.8, 1.2, 1],
-                x: [0, -50, -100, -50, 0],
-                y: [0, -50, -100, -50, 0],
-                borderRadius: ['0px', '50%', '50%', '0px', '0px'],
-                boxShadow: [
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
-                  '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
-                  '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
-                ],
-              }}
-              transition={{
-                delay: 0.2,
-                duration: 4,
-                ease: 'easeInOut',
-              }}
-            />
-            <DiamondMotion
-              initial={{
-                top: '48px',
-                scale: 1,
-              }}
-              animate={{
-                rotate: [45, 45, 90, 225, 225],
-                top: '48px',
-                opacity: [1, 0.5, 1, 1, 1],
-                scale: [0.25, 0.5, 0.8, 1.2, 1],
-                x: [0, -50, -100, -50, 0],
-                y: [0, 50, 100, 50, 0],
-                borderRadius: ['0px', '50%', '50%', '0px', '0px'],
-                boxShadow: [
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
-                  '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
-                  '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
-                ],
-              }}
-              transition={{
-                delay: 0.2,
-                duration: 4,
-                ease: 'easeInOut',
-              }}
-            />
-            <DiamondMotion
-              initial={{
-                left: '22px',
-                bottom: '-58px',
-                scale: 1,
-              }}
-              animate={{
-                rotate: [45, 45, 90, 225, 225],
-                left: '22px',
-                bottom: '-58px',
-                opacity: [1, 0.5, 1, 1, 1],
-                scale: [0.25, 0.5, 0.8, 1.2, 1],
-                x: [0, 50, 100, 50, 0],
-                y: [0, 50, 100, 50, 0],
-                borderRadius: ['0px', '50%', '50%', '0px', '0px'],
-                boxShadow: [
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
-                  '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
-                  '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
-                ],
-              }}
-              transition={{
-                delay: 0.2,
-                duration: 4,
-                ease: 'easeInOut',
-              }}
-            />
-            <DiamondMotion
-              initial={{
-                top: '0px',
-                scale: 1,
-              }}
-              animate={{
-                rotate: [45, 45, 90, 225, 225],
-                opacity: [1, 0.5, 1, 1, 1],
-                scale: [0.25, 0.5, 0.8, 1.2, 1],
-                x: [0, 50, 100, 50, 0],
-                y: [0, -50, -100, -50, 0],
-                borderRadius: ['0px', '50%', '50%', '0px', '0px'],
-                boxShadow: [
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
-                  '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
-                  '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
-                  '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
-                ],
-              }}
-              transition={{
-                delay: 0.2,
-                duration: 4,
-                ease: 'easeInOut',
-              }}
-            />
-          </Box>
-          {/* <Box
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="100%"
+          >
+            <Box position="relative">
+              <DiamondMotion
+                initial={{
+                  left: '-22px',
+                  bottom: '-58px',
+                  scale: 1,
+                }}
+                animate={{
+                  rotate: [45, 45, 90, 225, 225],
+                  left: '-22px',
+                  bottom: '-58px',
+                  opacity: [1, 0.5, 1, 1, 1],
+                  scale: [0.25, 0.5, 0.8, 1.2, 1],
+                  x: [0, -50, -100, -50, 0],
+                  y: [0, -50, -100, -50, 0],
+                  borderRadius: ['0px', '50%', '50%', '0px', '0px'],
+                  boxShadow: [
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
+                    '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
+                    '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
+                  ],
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 4,
+                  ease: 'easeInOut',
+                }}
+              />
+              <DiamondMotion
+                initial={{
+                  top: '48px',
+                  scale: 1,
+                }}
+                animate={{
+                  rotate: [45, 45, 90, 225, 225],
+                  top: '48px',
+                  opacity: [1, 0.5, 1, 1, 1],
+                  scale: [0.25, 0.5, 0.8, 1.2, 1],
+                  x: [0, -50, -100, -50, 0],
+                  y: [0, 50, 100, 50, 0],
+                  borderRadius: ['0px', '50%', '50%', '0px', '0px'],
+                  boxShadow: [
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
+                    '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
+                    '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
+                  ],
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 4,
+                  ease: 'easeInOut',
+                }}
+              />
+              <DiamondMotion
+                initial={{
+                  left: '22px',
+                  bottom: '-58px',
+                  scale: 1,
+                }}
+                animate={{
+                  rotate: [45, 45, 90, 225, 225],
+                  left: '22px',
+                  bottom: '-58px',
+                  opacity: [1, 0.5, 1, 1, 1],
+                  scale: [0.25, 0.5, 0.8, 1.2, 1],
+                  x: [0, 50, 100, 50, 0],
+                  y: [0, 50, 100, 50, 0],
+                  borderRadius: ['0px', '50%', '50%', '0px', '0px'],
+                  boxShadow: [
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
+                    '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
+                    '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
+                  ],
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 4,
+                  ease: 'easeInOut',
+                }}
+              />
+              <DiamondMotion
+                initial={{
+                  top: '0px',
+                  scale: 1,
+                }}
+                animate={{
+                  rotate: [45, 45, 90, 225, 225],
+                  opacity: [1, 0.5, 1, 1, 1],
+                  scale: [0.25, 0.5, 0.8, 1.2, 1],
+                  x: [0, 50, 100, 50, 0],
+                  y: [0, -50, -100, -50, 0],
+                  borderRadius: ['0px', '50%', '50%', '0px', '0px'],
+                  boxShadow: [
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 0 0 #fff, 0 0 0 0 #f0f, 0 0 0 0 #0ff',
+                    '0 0 15px 7px #fff, 0 0 25px 15px #f0f, 0 0 45px 22px #0ff',
+                    '0 0 30px 15px #fff, 0 0 50px 30px #f0f, 0 0 70px 45px #0ff',
+                    '0 0 0px 0px #fff, 0 0 0px 0px #f0f, 0 0 0px 0px #0ff',
+                  ],
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 4,
+                  ease: 'easeInOut',
+                }}
+              />
+            </Box>
+            {/* <Box
             as="img"
             alt="logo"
             src="logo.svg"
             height={ComponentHeights.header}
           /> */}
-          {/* </motion.div> */}
+            {/* </motion.div> */}
 
-          {/* <Box
+            {/* <Box
             bg="white"
             color="textInverted"
             fontSize={6}
@@ -170,7 +215,8 @@ export function Home() {
           >
             Anton Hallström
           </Box> */}
-        </Box>
+          </Box>
+        </Background>
       </Box>
       <Box py={['0px', 8, 8, 8]} px={['0px', 7, 8, 8]}></Box>
     </Route>
